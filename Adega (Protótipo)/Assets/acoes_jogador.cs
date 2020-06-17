dfd128d6d621f9a823e332d603be8eec3486b793
Ismael Dias <ismaelgedias@gmail.com>
2020-06-17 00:32:02 -0300

    Projeto DumontxCoronga
    Jogo criado com as crianças

---
+++ Adega (Protótipo)/Assets/acoes_jogador.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acoes_jogador : MonoBehaviour
+{
+    public int[] Uva_escolhida;
+    public int terreno_escolhido;
+    public float dinheiro;
+    public GameObject canvas1;
+    public bool pertoPilar = false;
+    public bool uiAberta;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && pertoPilar && !uiAberta)
+        {
+            canvas1.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            uiAberta = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && uiAberta)
+        {
+            canvas1.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            uiAberta = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Pilar"))
+        {
+            pertoPilar = true;
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Pilar"))
+        {
+            pertoPilar = false;
+        }
+        canvas1.SetActive(false); 
+    }
+}
+
---
+++ Adega (Protótipo)/Assets/mouse_lock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
+
